f9172943e11188fe6b71a28af47d757847b6e418
grosadi <groska.adam98@gmail.com>
2021-11-23 14:26:44 +0100

    Add project files.

---
+++ Thesis/Thesis/Pages/AddFacePage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Thesis.Service;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Thesis
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddFacePage : ContentPage
+    {        
+        private MediaFile Photo { get; set; }         
+        public AddFacePage()
+        {
+            InitializeComponent();
+            RegisterBtn.IsVisible = false;
+        }
+
+        private async void TakePhoto_Button_Clicked(object sender, EventArgs e)
+        {
+            if(FaceName.Text == null)
+            {
+                await DisplayAlert("", "Name is Required", "Ok");
+            }
+            else
+            {
+                Photo = await FaceService.GetMediaFileFromCamera();
+
+                if (Photo != null)
+                {
+                    RegisterBtn.IsVisible = true;
+                    PhotoImage.Source = ImageSource.FromStream(() => { return Photo.GetStream(); });
+                }
+            }            
+        }
+
+        private async void RegisterFace_Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var name = FaceName.Text;
+                await FaceService.AddFace(name, Photo);
+
+                await DisplayAlert("Great!", "Face added to Azure!", "Ok");
+
+                await Application.Current.MainPage.Navigation.PushAsync(new MainPage());
+             }
+            catch(Exception)
+            {
+                await DisplayAlert ("Sorry", "Something went wrong!", "Ok");
+            }
+        }
+    }
+}
---
+++ Thesis/Thesis/Pages/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Plugin.Fingerprint;
+using Plugin.Fingerprint.Abstractions;
+using System;
+using Xamarin.Forms;
+using Plugin.Media.Abstractions;
+using System.Linq;
+using Microsoft.Azure.CognitiveServices.Vision.Face;
+using System.Net.Http;
+using Thesis.Service;
+using System.IO;
+using Xamarin.Essentials;
+using Xamarin.Forms.Xaml;
+using Thesis.ViewModels;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+
+namespace Thesis
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {        
+        public MainPage()
+        {           
+            InitializeComponent();            
+        }
+
+        private async void Fingerprint_Button_Clicked(object sender, EventArgs e)
+        {      
+
+            bool supported = await CrossFingerprint.Current.IsAvailableAsync(true);
+            if (supported)
+            {
+                AuthenticationRequestConfiguration conf = new AuthenticationRequestConfiguration("Acces", "Acces");       
+
+                var result = await CrossFingerprint.Current.AuthenticateAsync(conf);
+
+                if (result.Authenticated)
+                {
+                    await DisplayAlert("Succes", "Authenticaton succesful!", "Ok");
+                }
+                else
+                {
+                    await DisplayAlert("Sorry", "Authenticaton failed!", "Ok");
+                }
+            }
+            else
+            {
+                await DisplayAlert("Sorry", "Not supported!", "Ok");
+            }
+        }
+
+        private async void DetectFace_Button_Clicked(object sender, EventArgs e)
+        {
+            var photo = await FaceService.GetMediaFileFromCamera();
+            var person = new Person();
+
+            try
+            {
+                person = await FaceService.Identify(photo);
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("Something went wrong", ex.Message, "Ok");
+            }            
+
+            if (person.Name != null)
+            {
+                await DisplayAlert("Identification Succesful!", $"Hello, {person.Name} !", "Ok");
+            }            
+            else
+            {
+                await DisplayAlert("Something went wrong", "Identification unsuccesful!", "Ok");
+            }
+        }       
+
+        private async void AddFace_Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddFacePage());
+        }
+    }
+}
